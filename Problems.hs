46d3197c2841dd726442cf03433f4b7f220e47b8
Bram Hoskin <hello@bram.gg>
2015-03-04 14:02:22 -0800

    Added problems/tests. New simpler description format as displayed tests add to explanation.

--- Problems.hs
+++ Problems.hs
@@ -11,15 +11,43 @@ data Problem = Problem { description :: String
 
 problems :: [Problem]
 problems = [ Problem
-                 "Make a function named 'myLast' that finds the last element of a list, assuming all lists are of length >= 1."
+                 "Make a function named 'myLast' that finds the last element of a list."
                  "myLast :: [a] -> a"
-                 [ ("myLast [1,2,3]", "3")
-                 , ("myLast ['x','y','z','q']", "'q'")
-                 , ("myLast [\"foo\"]", "\"foo\"") ]
+                 [ ("myLast [1,2,3,4]", "4")
+                 , ("myLast ['x','y','z']", "'z'") ]
 
            , Problem
-                 "Make a function named 'myButLast' that finds the second last element of a list, assuming all lists are of length >= 2."
+                 "Make a function named 'myButLast' that finds the second last element of a list."
                  "myButLast :: [a] -> a"
                  [ ("myButLast [1,2,3,4]", "3")
-                 , ("myButLast ['a'..'z']", "'y'")
-                 , ("myButLast \"qwerty\"", "'t'") ] ]
+                 , ("myButLast ['a'..'z']", "'y'") ]
+
+           , Problem
+                 "Make a function named 'elementAt' that finds the element of a list at a specific 1-based index."
+                 "elementAt :: [a] -> Int -> a"
+                 [ ("elementAt [1,2,3] 2", "2")
+                 , ("elementAt \"haskell\" 5", "'e'") ]
+
+           , Problem
+                 "Make a functtion named 'myLength' that finds the number of elements in a list."
+                 "myLength :: [a] -> Int"
+                 [ ("myLength [123, 456, 789]", "2")
+                 , ("myLength \"Hello, World!\"", "13") ]
+
+           , Problem
+                "Make a function called 'myReverse' that reverses a list."
+                "myReverse :: (Eq a) => [a] -> [a]"
+                [ ("myReverse \"A man, a plan, a canal, panama!\"", "\"!amanap ,lanac a ,nalp a ,nam A\"")
+                , ("myReverse [1,2,3,4]", "[4,3,2,1]") ]
+           , Problem
+                "Make a function called 'isPalindrome' that finds whether a list is a palindrome (is the same read forward and backward)."
+                "isPalindrome :: (Eq a) => [a] -> Bool"
+                [ ("isPalindrome [1,2,3]", "False")
+                , ("isPalindrome \"madamimadam\"", "True")
+                , ("isPalindrome [1,2,4,8,16,8,4,2,1]", "True") ]
+           , Problem
+                "Make a function called 'flatten' that flattens a nested list structure."
+                "data NestedList a = Elem a | List [NestedList a]\nflatten :: NestedList a -> [a]"
+                [ ("flatten (Elem 5)", "[5]")
+                , ("flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])", "[1,2,3,4,5]")
+                , ("flatten (List [])", "[]") ] ]
