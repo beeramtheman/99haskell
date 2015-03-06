e493e098ace2a6f833113f55a8de3cd33597475a
Bram Hoskin <hello@bram.gg>
2015-03-06 03:05:52 -0800

    more problems, first 10 written

--- Problems.hs
+++ Problems.hs
@@ -39,15 +39,35 @@ problems = [ Problem
                 "myReverse :: (Eq a) => [a] -> [a]"
                 [ ("myReverse \"A man, a plan, a canal, panama!\"", "\"!amanap ,lanac a ,nalp a ,nam A\"")
                 , ("myReverse [1,2,3,4]", "[4,3,2,1]") ]
+
            , Problem
                 "Make a function called 'isPalindrome' that finds whether a list is a palindrome (is the same read forward and backward)."
                 "isPalindrome :: (Eq a) => [a] -> Bool"
                 [ ("isPalindrome [1,2,3]", "False")
                 , ("isPalindrome \"madamimadam\"", "True")
                 , ("isPalindrome [1,2,4,8,16,8,4,2,1]", "True") ]
+
            , Problem
                 "Make a function called 'flatten' that flattens a nested list structure."
                 "data NestedList a = Elem a | List [NestedList a]\nflatten :: NestedList a -> [a]"
                 [ ("flatten (Elem 5)", "[5]")
                 , ("flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])", "[1,2,3,4,5]")
-                , ("flatten (List [])", "[]") ] ]
+                , ("flatten (List [])", "[]") ]
+
+           , Problem
+                "Make a function called 'compress' removes consecutive duplicates of elements in a list without affecting the order."
+                "compress :: Eq a => [a] -> [a]"
+                [ ("compress \"aaaabccaadeeee\"", "\"abcade\"")
+                , ("compress [1,1,2,3,1,2,2,3]", "[1,2,3,1,2,3]") ]
+
+           , Problem
+                "Make a function called 'pack' that packs consecutive duplicates and standalone elements into sublists."
+                "pack :: Eq a => [a] -> [[a]]"
+                [ ("pack \"aaaabccaadeeee\"", "[\"aaaa\",\"b\",\"cc\",\"aa\",\"d\",\"eeee\"]")
+                , ("pack [1,1,2,3,1,2,2,3]", "[[1,1],[2],[3],[1],[2,2],[3]]") ]
+
+           , Problem
+                "Make a function called 'encode' that implements run-length encoding on a list. That is, consecutive duplicates and standalone elements are replaced with tuples of the format (number of occurences of element, element itself)."
+                "encode :: Eq a => [a] -> [(Int, a)]"
+                [ ("encode \"aaaabccaadeeee\"", "[(4,'a'),(1,'b'),(2,'c'),(2,'a'),(1,'d'),(4,'e')]")
+                , ("encode [1,1,2,3,1,2,2,3]", "[(2,1),(1,2),(1,3),(1,1),(2,2),(1,3)]") ] ]
