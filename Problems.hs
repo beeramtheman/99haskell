c6f7bd8761dd9b0a1d59709979ae30b0a492435f
Bram Hoskin <hello@bram.gg>
2015-03-09 15:40:39 -0700

    removed pointless myReverse class constraint

--- Problems.hs
+++ Problems.hs
@@ -36,7 +36,7 @@ problems = [ Problem
 
            , Problem
                 "Make a function called 'myReverse' that reverses a list."
-                "myReverse :: (Eq a) => [a] -> [a]"
+                "myReverse :: [a] -> [a]"
                 [ ("myReverse \"A man, a plan, a canal, panama!\"", "\"!amanap ,lanac a ,nalp a ,nam A\"")
                 , ("myReverse [1,2,3,4]", "[4,3,2,1]") ]
 
